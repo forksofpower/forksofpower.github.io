2f28584385ffff519220e9dee66e7ff31efd5df6
Patrick Jones <forksofpower@gmail.com>
2018-08-09 13:34:21 -0500

    Updates

--- src/actions/types.js
+++
@@ -1,3 +0,0 @@
-export const TOGGLE_MENU = 'TOGGLE_MENU';
-export const HIDE_MENU = 'HIDE_MENU';
-export const SHOW_MENU = 'SHOW_MENU';
--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker, { unregister as unregisterServiceWorker } from './registerServiceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-// registerServiceWorker();
-unregisterServiceWorker();
